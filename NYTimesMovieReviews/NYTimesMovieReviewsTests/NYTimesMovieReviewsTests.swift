41bfee38c373805d2d776c82d4c6366db32ce662
Viola Pirri <violapirri@gmail.com>
2017-09-26 18:13:16 -0400

    Adding unit test

--- NYTimesMovieReviews/NYTimesMovieReviewsTests/NYTimesMovieReviewsTests.swift
+++ NYTimesMovieReviews/NYTimesMovieReviewsTests/NYTimesMovieReviewsTests.swift
@@ -11,26 +11,41 @@ import XCTest
 
 class NYTimesMovieReviewsTests: XCTestCase {
     
+    var sessionUnderTest = URLSession()
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        sessionUnderTest = URLSession(configuration: URLSessionConfiguration.default)
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testCallToNYTimesAPI() {
+        // given
+        let url = URL(string: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=68b0070d4910436cb1b211e4305f310d&critics-pick=Y")
+        
+        let promise = expectation(description: "Status code: 200")
+        
+        // when
+        let dataTask = sessionUnderTest.dataTask(with: url!) { data, response, error in
+            // then
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+                return
+            } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if statusCode == 200 {
+                    promise.fulfill()
+                } else {
+                    XCTFail("Status code: \(statusCode)")
+                }
+            }
         }
+        dataTask.resume()
+        
+        waitForExpectations(timeout: 5, handler: nil)
     }
     
 }
